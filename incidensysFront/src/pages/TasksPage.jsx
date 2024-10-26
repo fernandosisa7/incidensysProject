c700b9ac16280addb4f0a1e0d87e66bd1b8873ae
Fernando Sisa <fernando7@gmail.com>
2024-10-26 15:15:33 -0500

    actualiza pag de tareas

--- incidensysFront/src/pages/TasksPage.jsx
+++ incidensysFront/src/pages/TasksPage.jsx
@@ -1,10 +1,10 @@
+import dayjs from 'dayjs';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import TaskCard from '../components/TaskCard';
 import { useTasks } from '../context/TasksContext';
 
 const TasksPage = () => {
-    const { getTasks, tasks } = useTasks();
+    const { getTasks, tasks, deleteTask } = useTasks();
 
     useEffect(() => {
         getTasks();
@@ -16,10 +16,28 @@ const TasksPage = () => {
         <Link to='/add-task' className="bg-green-600 px-4 py-1 rounded-sm">
             Add task
         </Link>
-        
+
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-2'>
             {tasks.map(task => (
-                <TaskCard task={task} key={task._id} />
+                <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+                    <header className="flex justify-between">
+                        <h1 className="text-2xl font-bold">{task.title}</h1>
+                        <div className="flex gap-x-2 items-center">
+                            <button
+                                className='w-full bg-red-500 text-white px-4 py-2 rounded-md'
+                                onClick={() => { deleteTask(task._id) }}>
+                                delete
+                            </button>
+                            <Link to={`/tasks/${task._id}`} className='w-full bg-blue-500 text-white px-4 py-2 rounded-md'>
+                                edit
+                            </Link>
+                        </div>
+                    </header>
+                    <p className="text-slate-300">{task.description}</p>
+                    <p>
+                        {dayjs(task.date).utc().format('DD/MM/YYYY')}
+                    </p>
+                </div >
             ))}
         </div>
     </>
