ffbfccee0e8ec38b98c6fd3adc929a1fdea54195
Fernando Sisa <fernando7@gmail.com>
2024-11-01 15:22:31 -0500

    actualiza pag de examples

--- incidensysBack/src/schemas/example.schema.js
+++ incidensysBack/src/schemas/example.schema.js
@@ -6,7 +6,7 @@ export const saveExampleSchema = z.object({
         required_error: 'Title is required'
     }),
     description: z.string({
-        required_error: 'Description must be a string'
+        required_error: 'Description is required'
     }),
     date: z.string().datetime().optional(),
 });
